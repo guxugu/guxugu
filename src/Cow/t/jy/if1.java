fd6fcab74bd7e1dbe3d7b3df2e131f6315b2298d
turbo <turbo@DESKTOP-054C8MB>
2018-04-05 21:58:03 +0900

    ㅇㅇㅇ

--- src/Cow/t/jy/if1.java
+++ src/Cow/t/jy/if1.java
@@ -11,7 +11,8 @@ public class if1 {
 		char Hakjum;
 		Jumsu=Integer.parseInt(JOptionPane.showInputDialog("점수 입력:"));
 		boolean result = Jumsu>=90;
-		//boolean 일단 보류 광역변수로 주로 활용은 한다
+		//boolean 거짓과 참을 정하는 역활이고 그 판단을 하는 것은 아님
+		// 그러니까 내가 일단 말하는게 참이다 라고 광역 선포 하고 나서 나중에 가져다가 씀 일단 보류 광역변수로 주로 활용은 한다
 		if (result){Hakjum='A';}
 		else if(Jumsu >=80){Hakjum='B';}
 		else if(Jumsu >=70){Hakjum='C';}
